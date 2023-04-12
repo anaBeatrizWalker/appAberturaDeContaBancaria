9813f31a136747912d6bc8ab5f4f0e08a52f7b8c
anaBeatrizWalker <anabiawalker@gmail.com>
2023-04-12 08:05:31 -0300

    Alterando aplicação para componente funcional

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Text, View, StyleSheet, TextInput, Switch, Button } from 'react-native';
 
 import {styles} from './styles.js';
@@ -6,91 +6,82 @@ import {styles} from './styles.js';
 import {Picker} from '@react-native-picker/picker';
 import Slider from '@react-native-community/slider';
 
-export default class App extends Component {
+export default function App(){
 
-  constructor(props){
-    super(props);
-    this.state = {
-      nome: '',
-      idade: 0,
-      sexo: '',
-      escolaridade: '',
-      limite: 0,
-      brasileiro: ''
-    }
-    this.enviar = this.enviar.bind(this);
-  }
+  const [nome, setNome] = useState('');
+  const [idade, setIdade] = useState(0);
+  const [sexo, setSexo] = useState('');
+  const [escolaridade, setEscolaridade] = useState('');
+  const [limite, setLimite] = useState(0);
+  const [brasileiro, setBrasileiro] = useState('');
 
-  enviar(){
-    this.setState({
-      nome: this.state.nome,
-      idade: this.state.idade,
-      sexo: this.state.sexo,
-      escolaridade: this.state.escolaridade,
-      limite: this.state.limite,
-      brasileiro: this.state.brasileiro
-    });
-    
-    console.log(this.state.nome);
+  function enviar(){
+    setNome(nome);
+    setIdade(idade);
+    setSexo(sexo);
+    setEscolaridade(escolaridade);
+    setLimite(limite);
+    setBrasileiro(brasileiro);
+
     let elemento = document.getElementById("resultado");
     elemento.style.display = "block";
   }
 
-  render(){
-      return (
-        <View style={styles.container}>
-          <Text style={styles.titulo}>Abertura de Conta</Text>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Abertura de Conta</Text>
 
-          <Text style={styles.label}>Nome:</Text>
-          <TextInput style={styles.inputs} placeholder="Digite seu nome"  onChangeText={(a) => this.setState({nome: a})} value={this.state.nome}></TextInput>
+      <Text style={styles.label}>Nome:</Text>
+      <TextInput style={styles.inputs} placeholder="Digite seu nome"  onChangeText={(value) => setNome(value)} value={nome}></TextInput>
 
-          <Text style={styles.label}>Idade:</Text>
-          <TextInput style={styles.inputs} placeholder="Digite sua idade: " onChangeText={(a) => this.setState({idade: a})}></TextInput>
+      <Text style={styles.label}>Idade:</Text>
+      <TextInput style={styles.inputs} placeholder="Digite sua idade: " onChangeText={(value) =>setIdade(value)}></TextInput>
 
-          <Text style={styles.label}>Sexo:</Text>
-          <Picker selectedValue={this.state.sexo} style={styles.inputs} onValueChange={(value, index) => this.setState({sexo: value})}>
-            <Picker.Item key={1} value={"Feminino"} label="Feminino"/>
-            <Picker.Item key={2} value={"Masculino"} label="Masculino"/>
-            <Picker.Item key={3} value={"Outro"} label="Outro" />
-          </Picker>
+      <Text style={styles.label}>Sexo:</Text>
+      <Picker selectedValue={sexo} style={styles.inputs} onValueChange={(value, index) => setSexo(value)}>
+        <Picker.Item key={1} value={""} label="Selecione: " />
+        <Picker.Item key={2} value={"Feminino"} label="Feminino"/>
+        <Picker.Item key={3} value={"Masculino"} label="Masculino"/>
+        <Picker.Item key={4} value={"Outro"} label="Outro" />
+      </Picker>
 
-          <Text style={styles.label}>Escolaridade:</Text>
-          <Picker style={styles.inputs} onValueChange={(value, index) => this.setState({escolaridade: value})}>
-            <Picker.Item key={1} value={"Ensino Médio"} label="Ensino Médio" />
-            <Picker.Item key={2} value={"Graduação"} label="Graduação" />
-            <Picker.Item key={3} value={"Pós-Graduação"} label="Pós-Graduação" />
-            <Picker.Item key={4} value={"Doutorado"} label="Doutorado" />
-            <Picker.Item key={5} value={"Mestrado"} label="Mestrado" />
-          </Picker>
+      <Text style={styles.label}>Escolaridade:</Text>
+      <Picker style={styles.inputs} onValueChange={(value, index) => setEscolaridade(value)}>
+        <Picker.Item key={1} value={""} label="Selecione: " />
+        <Picker.Item key={2} value={"Ensino Médio"} label="Ensino Médio" />
+        <Picker.Item key={3} value={"Graduação"} label="Graduação" />
+        <Picker.Item key={4} value={"Pós-Graduação"} label="Pós-Graduação" />
+        <Picker.Item key={5} value={"Doutorado"} label="Doutorado" />
+        <Picker.Item key={6} value={"Mestrado"} label="Mestrado" />
+      </Picker>
 
-          <Text style={styles.label}>Limite:</Text>
-          <Slider style={styles.inputs} minimumValue={0} maximumValue={10} onValueChange={(value) => this.setState({limite: value})} value={this.state.limite.toFixed(2)}/>
-          <Text style={styles.legend}>{this.state.limite.toFixed(2)}</Text>
+      <Text style={styles.label}>Limite:</Text>
+      <Slider style={styles.inputs} minimumValue={0} maximumValue={10} onValueChange={(value) => setLimite(value)} value={limite.toFixed(2)}/>
+      <Text style={styles.legend}>{limite.toFixed(2)}</Text>
 
-          <Text style={styles.label}>Brasileiro:</Text>
-          <Switch style={styles.switchs} value={this.state.brasileiro} onValueChange={ (valor) => this.setState({brasileiro: valor})}/>
-          <Text>{(this.state.brasileiro) ? "Sim" : "Não"}</Text>
+      <Text style={styles.label}>Brasileiro:</Text>
+      <Switch style={styles.switchs} value={brasileiro} onValueChange={ (value) => setBrasileiro(value)}/>
+      <Text>{(brasileiro) ? "Sim" : "Não"}</Text>
 
-          <View style={styles.button}>
-            <Button title="Enviar" onPress={this.enviar} color="#32CD32"></Button>
-          </View>
+      <View style={styles.button}>
+        <Button title="Enviar" onPress={enviar} color="#32CD32"></Button>
+      </View>
 
-          <div id="resultado" style={{display: "none"}}>
-              <Text style={styles.titulo}>Dados informados: </Text>
-              <br></br>
-              <Text style={styles.textoResultado}>Nome: {this.state.nome}</Text>
-              <br></br>
-              <Text style={styles.textoResultado}>Idade: {this.state.idade}</Text>
-              <br></br>
-              <Text style={styles.textoResultado}>Sexo: {this.state.sexo}</Text>
-              <br></br>
-              <Text style={styles.textoResultado}>Escolaridade: {this.state.escolaridade}</Text>
-              <br></br>
-              <Text style={styles.textoResultado}>Limite: {this.state.limite.toFixed(2)}</Text>
-              <br></br>
-              <Text style={styles.textoResultado}>É brasileiro: {(this.state.brasileiro) ? "Sim" : "Não"}</Text>
-          </div>
-        </View>
-      );
-  }
+      <div id="resultado" style={{display: "none"}}>
+          <Text style={styles.titulo}>Dados informados: </Text>
+          <br></br>
+          <Text style={styles.textoResultado}>Nome: {nome}</Text>
+          <br></br>
+          <Text style={styles.textoResultado}>Idade: {idade}</Text>
+          <br></br>
+          <Text style={styles.textoResultado}>Sexo: {sexo}</Text>
+          <br></br>
+          <Text style={styles.textoResultado}>Escolaridade: {escolaridade}</Text>
+          <br></br>
+          <Text style={styles.textoResultado}>Limite: {limite.toFixed(2)}</Text>
+          <br></br>
+          <Text style={styles.textoResultado}>É brasileiro: {(brasileiro) ? "Sim" : "Não"}</Text>
+      </div>
+    </View>
+  );
 }
